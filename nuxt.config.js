654385431bc646d1ed8023bcba06b75e732e55e2
iepngs <iepngs@aliyun.com>
2020-08-21 17:10:18 +0800

    fix proxy

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,26 +66,15 @@ export default {
 
 
     // axios: {
-    //     retry: {retries: 3},
-    //     proxy: true, // 表示开启代理
-    //     prefix: '/api', // 表示给请求url加个前缀 /api
-    //     credentials: true // 表示跨域请求时是否需要使用凭证
+    //     // retry: {retries: 3},
+    //     proxy: true,
+    //     credentials: false,
+    //     timeout: 5000,
     // },
 
-    proxy: [
-        ["/api", {target: "https://h5api.zhefengle.cn", pathRewrite: {'^/api/': ''} }]
-    ],
-
-    // proxy: {
-    //     '/api': {
-    //         target: 'https://h5api.zhefengle.cn', // 目标接口域名
-    //         pathRewrite: {
-    //             '^/api': '/', // 把 /api 替换成 /
-    //             changeOrigin: true // 表示是否跨域
-    //         }
-    //     }
-    // },
-
+    proxy: {
+        "/api": { target: "https://h5api.zhefengle.cn", pathRewrite: { '^/api/': '',  changeOrigin: true } }
+    },
 
     /*
     ** Build configuration
